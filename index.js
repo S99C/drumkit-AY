705aa9a7df0158f52a797774651fa7bd153c0977
Saptarshi Chakraborty <saptarshi1999chakraborty@gmail.com>
2021-08-27 07:46:21 +0530

    fix

--- index.js
+++ index.js
@@ -21,25 +21,25 @@ document.addEventListener("keypress", function(evt) {
 //SECTION TO PLAY SOUNDS
 function makeNoise(key) {
     switch (key) {
-        case "w": var tom1 = new Audio('sounds/tom-1.mp3');
+        case "w": var tom1 = new Audio('tom-1.mp3');
             tom1.play();
             break;
-        case "a": var tom2 = new Audio('sounds/tom-2.mp3');
+        case "a": var tom2 = new Audio('tom-2.mp3');
             tom2.play();
             break;
-        case "s": var tom3 = new Audio('sounds/tom-3.mp3');
+        case "s": var tom3 = new Audio('tom-3.mp3');
             tom3.play();
             break;
-        case "d": var tom4 = new Audio('sounds/tom-4.mp3');
+        case "d": var tom4 = new Audio('tom-4.mp3');
             tom4.play();
             break;
-        case "j": var crash = new Audio('sounds/crash.mp3');
+        case "j": var crash = new Audio('crash.mp3');
             crash.play();
             break;
-        case "k": var kick = new Audio('sounds/kick-bass.mp3');
+        case "k": var kick = new Audio('kick-bass.mp3');
             kick.play();
             break;
-        case "l": var snare = new Audio('sounds/snare.mp3');
+        case "l": var snare = new Audio('snare.mp3');
             snare.play();
             break;
         default: console.log(key_id.innerHTML);
